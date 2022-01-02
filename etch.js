30965a4b36f79f1c6f3bd0a72e0275164e436bc7
Winston <winstonjupchurch@gmail.com>
2022-01-02 11:41:16 -0600

    forgot to push changes from yesterday no idea what im adding rn

--- etch.js
+++ etch.js
@@ -1,35 +1,43 @@
 //set names
 const container = document.getElementById('container');
-let cols = container.getElementsByClassName('column');
-let cells = container.getElementsByClassName('cell');
-let cellStyle = container.getElementsByClassName('cell').style;
-cells.addEventListener('mouseover', draw());
+
+
+
+
 //creates n rows
 function makeCols(n) {
 for(i = 0; i < n; i++){
-    const column = document.createElement('column');
+    const column = document.createElement('div');
     column.className = 'column';
     container.appendChild(column)
 }
 }
 
 //makes n cells, appends to col
-function makeCells(){
+function makeCells(n){
     for(let i = 0; i < cols.length; i++) {
-        for(let i = 0; i < 16; i++) {
-            const cell = document.createElement('cell');
+        for(let i = 0; i < n; i++) {
+            const cell = document.createElement('div');
             cols[i].appendChild(cell).className = 'cell';
         }
 }
 }
+//post function grouping variables
+let cols = container.getElementsByClassName('column');
+let cells = container.getElementsByClassName('cell');
+let cellsTest = document.querySelectorAll('.cell');
+
+//make a 16x16 grid
 function defaultGrid(){
     makeCols(16);
     makeCells(16);
 }
-function draw() {
-   cellStyle.backgroundColor = 'black';
-   cellStyle.border = '1px solid black'
+//make a n x n grid
+function test(n, i) {
+    makeCols(n);
+    makeCells(i);
 }
-//running idea : create a togglable element for cells that changes background color to black, triggered by mouseover eventlistener. cell.addEventListener('mouseover') {
-//event.target.style.color = 'black';
-//}
+//for mouseover event listener. switch bgcolor of cells 
+for (single of cells) {
+    cell.addEventListener("mouseover", draw => {cell.style.backgroundColor = black})
+}
