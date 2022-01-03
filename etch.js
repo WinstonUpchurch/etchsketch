63d0597669bb3911d4586ff5a3414518bad13d2c
Winston <winstonjupchurch@gmail.com>
2022-01-02 18:25:14 -0600

    wrote a nice commit msg before i found out i fucked something up now dont feel likt it

--- etch.js
+++ etch.js
@@ -1,10 +1,10 @@
 //set names
 const container = document.getElementById('container');
+//grouping variables
+let cols = container.getElementsByClassName('column');
+let cells = container.getElementsByClassName('cell');
 
-
-
-
-//creates n rows
+//creates (n) rows
 function makeCols(n) {
 for(i = 0; i < n; i++){
     const column = document.createElement('div');
@@ -13,7 +13,7 @@ for(i = 0; i < n; i++){
 }
 }
 
-//makes n cells, appends to col
+//makes n cells, appends cells to columns
 function makeCells(n){
     for(let i = 0; i < cols.length; i++) {
         for(let i = 0; i < n; i++) {
@@ -22,27 +22,66 @@ function makeCells(n){
         }
 }
 }
-//post function grouping variables
-let cols = container.getElementsByClassName('column');
-let cells = container.getElementsByClassName('cell');
-let cellsTest = document.querySelectorAll('.cell');
 
 //make a 16x16 grid
 function defaultGrid(){
     makeCols(16);
     makeCells(16);
 }
-//make a n x n grid
-function test(n, i) {
-    makeCols(n);
-    makeCells(i);
+//makes a grid taking user input via prompt, then checks validty of input, then creates divs. finally preps divs for etching. 
+function customGrid() {
+    let userInput = prompt('Enter an even number between 16-100');
+    // conditional call of reset if a grid is already present and the customGrid is called again
+    if(container.firstChild){
+        reset(container)
+    } else return
+    //check that users input is valid
+    if (userInput > 100) {
+        alert('please enter a value less than 101')
+    } else if (userInput < 16) {
+        alert('please enter a value over 15')
+    } else {
+        makeCols(userInput);
+        makeCells(userInput);
+    }
+    //adds event listeners to cells.
+    prep();
 }
+    
+
 //for mouseover event listener. switch bgcolor of cells 
 let draw = function(){
     cells[this].style.backgroundColor = 'black';
 }
+//prep is ran on creation of the grid assigning each cell the eventlistener
 function prep() {
     for(i = 0; i < cells.length; i++) {
-        cells[i].addEventListener('mouseover', function(){this.style.backgroundColor = 'black'})
+        cells[i].addEventListener('mouseover', function(){
+            this.style.backgroundColor = 'black';
+        });
+    }
+}
+//funky mode
+function randInt(max) {
+    return Math.floor(Math.random() * max);
+}
+let int = randInt(255)
+function crazy() {
+    let R = randInt(255);
+    let G = randInt(255);
+    let B = randInt(255);
+    let A = randInt(255);
+    for(i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('mouseover', function(){
+            randInt(255)
+            this.style.backgroundColor = `rgba(${R}, ${G}, ${B}, ${A})`;
+        });
+    }
+}
+
+//reset grid function
+function reset(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
     }
 }
